dcbf749ea221f6d57d5e0d5d1310273d0bf360cd
anil1791 <1234anil2@gmail.com>
2023-09-24 09:56:59 +0000

    random no

--- Begin_1/string_interpolation.js
+++ Begin_1/string_interpolation.js
@@ -2,4 +2,19 @@ const firstName = "anil"
 const secondName  = "Kumar"
 
 
-console.log(`my name is ${firstName} ${secondName}`)
+// console.log(`my name is ${firstName.toUpperCase()} ${secondName}`)
+// console.log(firstName[0]);
+
+const clat = new String('anil-kuamr')
+const newstring = clat.substring(0,clat.indexOf('-'))
+//console.log(clat.replace('-',' '))
+
+
+const  no =12321327
+//console.log(no.toLocaleString('en-IN'));
+
+const min = 6
+const max = 12
+
+const rno = Math.floor(Math.random()*(max - min +1)+min)
+console.log(rno);
