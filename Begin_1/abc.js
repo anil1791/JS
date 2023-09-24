280835e33b95fd98990c35c90af1ea0f41a1c40c
anil1791 <1234anil2@gmail.com>
2023-09-24 05:25:20 +0000

    more about symbols

--- Begin_1/abc.js
+++ Begin_1/abc.js
@@ -1,1 +1,13 @@
-console.log("Anil")
+"use strict" //use it for newer version
+
+//null is an object
+let no = "22a"
+let newno = Number(no)
+console.log(typeof newno)
+console.log(newno);
+
+var nn = false
+nn = "as";
+console.log(typeof nn)
+
+console.log("September");
