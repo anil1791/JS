bc15da28df058fe65cd5433dbf4871d04b7f1cb5
anil1791 <1234anil2@gmail.com>
2023-09-25 17:45:12 +0000

    added object destructuring

--- Begin_1/objects.js
+++ Begin_1/objects.js
@@ -4,8 +4,23 @@ const sym1 = Symbol("asdf")
 
 const myobj = {
     name : "anil",
-    [sym1] : "23432"            //remember this symbol syntax
-    
+    [sym1] : "23432",            //remember this symbol syntax
+    current_full_job: "SE",
+    phone: "324123141"
 }
 
-console.log(typeof myobj[sym1]);
+
+const ob1 = { a: "123",b: "dfk1",c : "dfd"}
+const obj2 = {g: "fdf",h : "popd"}
+
+const final_obj = {...ob1,...obj2}//Object.assign({},ob1,obj2)
+
+const arr1 = Object.entries(final_obj)
+//console.log(arr1)
+
+//object destructuring
+
+const {current_full_job: job} = myobj
+
+console.log(job);
+
